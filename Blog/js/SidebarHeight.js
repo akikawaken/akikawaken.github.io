9ae3e29068917ee385a46cf6149188942d807f9c
tsubaki <akikawatoiawase@outlook.jp>
2024-09-06 07:16:02 +0900

    th miyasuku sita

--- Blog/js/SidebarHeight.js
+++ Blog/js/SidebarHeight.js
@@ -2,3 +2,15 @@ window.onload = function() {
     var mainHeight = document.querySelector('.main').offsetHeight;
     document.querySelector('.sidebar').style.height = mainHeight + 'px';
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    const heads = document.querySelectorAll("th.head");
+    heads.forEach(head => {
+      const nextTh = head.nextElementSibling;
+      if (nextTh) {
+        const headHeight = head.offsetHeight;
+        const arrowSize = headHeight / 2;
+        nextTh.style.setProperty('--arrow-size', `${arrowSize}px`);
+      }
+    });
+  });
